3b7add86dc60197f34e5291e7190ae981919e173
BeokBeok <kekemusa37@nate.com>
2020-06-13 11:00:31 +0900

    [추가] dagger hilt 디펜던시

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -5,6 +5,7 @@ object Version {
 object ProjectConfig {
     const val GRADLE = "com.android.tools.build:gradle:4.0.0"
     const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.KOTLIN}"
+    const val HILT_ANDROID_GRADLE_PLUGIN = "com.google.dagger:hilt-android-gradle-plugin:2.28-alpha"
 }
 
 object AndroidConfig {
@@ -17,11 +18,16 @@ object AndroidConfig {
 }
 
 object Dependencies {
+    private const val HILT_VER = "2.28-alpha"
+
     const val KOTLIN_STDLIB = "org.jetbrains.kotlin:kotlin-stdlib:${Version.KOTLIN}"
 
     const val MATERIAL = "com.google.android.material:material:1.1.0"
     const val CORE_KTX = "androidx.core:core-ktx:1.3.0"
     const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+    const val HILT_ANDROID = "com.google.dagger:hilt-android:$HILT_VER"
+    const val HILT_ANDROID_COMPILER = "com.google.dagger:hilt-android-compiler:$HILT_VER"
 }
 
 object TestDependencies {
