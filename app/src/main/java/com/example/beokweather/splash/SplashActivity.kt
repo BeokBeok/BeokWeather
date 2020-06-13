b2b72c167c85dc99b6bac134e34ac02f2667fb68
BeokBeok <kekemusa37@nate.com>
2020-06-13 18:15:34 +0900

    [적용] Location 권한

---
+++ app/src/main/java/com/example/beokweather/splash/SplashActivity.kt
@@ -0,0 +1,93 @@
+package com.example.beokweather.splash
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.beokweather.R
+import com.example.beokweather.ext.isNotValidLocationPermission
+import com.example.beokweather.ext.startActivity
+import com.example.beokweather.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (isNotValidLocationPermission()) {
+            if (requestLocationPermissionIfDeniedBefore()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    0
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    0
+                )
+            }
+        } else {
+            startActivity<MainActivity>()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (isNotValidLocationPermission()) {
+            AlertDialog.Builder(this)
+                .setTitle(getString(R.string.dialog_title_permission_settings))
+                .setMessage(getString(R.string.msg_unavailable_app_because_permission_denied))
+                .setPositiveButton(getString(R.string.label_go_permission_settings)) { _, _ ->
+                    try {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                            .setData(Uri.parse("package:$packageName"))
+                        startActivity(intent)
+                    } catch (e: ActivityNotFoundException) {
+                        val intent = Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS)
+                        startActivity(intent)
+                    }
+                }
+                .setNegativeButton(getString(R.string.label_cancel)) { _, _ ->
+                    Toast.makeText(
+                        this,
+                        getString(R.string.msg_app_finish),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finish()
+                }
+                .create()
+                .show()
+        } else {
+            startActivity<MainActivity>()
+        }
+    }
+
+    private fun requestLocationPermissionIfDeniedBefore(): Boolean =
+        ActivityCompat.shouldShowRequestPermissionRationale(
+            this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        ).and(
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+}
