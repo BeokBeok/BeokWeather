77594200bb2d00ad3d3f226209fc99757de90b13
BeokBeok <kekemusa37@nate.com>
2020-06-13 11:45:38 +0900

    [추가] Network 관련 di 모듈

---
+++ app/src/main/java/com/example/beokweather/Beokplication.kt
@@ -0,0 +1,7 @@
+package com.example.beokweather
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Beokplication : Application()
---
+++ app/src/main/java/com/example/beokweather/di/OpenWeatherMapInterceptor.kt
@@ -0,0 +1,24 @@
+package com.example.beokweather.di
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class OpenWeatherMapInterceptor @Inject constructor() : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val url = chain.request().url()
+            .newBuilder()
+            .addQueryParameter("appid", API_KEY)
+            .build()
+
+        val request = chain.request().newBuilder().url(url).build()
+        return chain.proceed(request)
+    }
+
+    companion object {
+        private const val API_KEY = "2e6c1e481f600b6cb9166a0d297da31a"
+    }
+}
