f9ae7590e1247fb4af16518d4f1e5d06815f9f5d
BeokBeok <kekemusa37@nate.com>
2020-06-15 00:15:38 +0900

    [적용] startActivity extension in bundle parameter

--- app/src/main/java/com/example/beokweather/ext/ActivityExt.kt
+++ app/src/main/java/com/example/beokweather/ext/ActivityExt.kt
@@ -2,8 +2,12 @@ package com.example.beokweather.ext
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
 
-inline fun <reified T : Activity> Activity.startActivity() {
+inline fun <reified T : Activity> Activity.startActivity(bundle: Bundle? = null) {
     val intent = Intent(this, T::class.java)
+    if (bundle != null) {
+        intent.putExtras(bundle)
+    }
     startActivity(intent)
 }
