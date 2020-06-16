3e928a1a9577f4924ba3e750e99434518804a62f
BeokBeok <kekemusa37@nate.com>
2020-06-16 23:10:58 +0900

    [정리] convertToWeatherItem function

--- app/src/main/java/com/example/beokweather/util/ConvertUtil.kt
+++ app/src/main/java/com/example/beokweather/util/ConvertUtil.kt
@@ -2,6 +2,7 @@ package com.example.beokweather.util
 
 import android.annotation.SuppressLint
 import com.example.beokweather.main.model.Forecast
+import com.example.beokweather.main.model.ListItem
 import com.example.beokweather.main.model.WeatherItem
 import java.text.SimpleDateFormat
 import java.util.*
@@ -11,38 +12,46 @@ object ConvertUtil {
     @SuppressLint("SimpleDateFormat")
     private val dateFormatForYYYYMMDD = SimpleDateFormat("yyyy-MM-dd")
 
-    fun convertToWeatherItem(forecastList: List<Forecast>): List<WeatherItem> {
-        if (forecastList.isEmpty()) return emptyList()
+    fun convertToWeatherItem(weatherList: List<Forecast>): List<WeatherItem> {
+        if (weatherList.isEmpty()) return emptyList()
 
         val now = dateFormatForYYYYMMDD.format(Date())
-        val listItem = mutableListOf<WeatherItem>()
-        val forecastByDate = forecastList[1].list.groupBy { it.dtTxt.substringBefore(" ") }
-        listItem.add(
-            WeatherItem(
-                name = forecastList[0].name,
-                date = "Today",
-                icon = forecastList[0].list[0].icon,
-                temp = forecastList[0].list[0].main.temp,
-                tempMin = forecastList[0].list[0].main.tempMin,
-                tempMax = forecastList[0].list[0].main.tempMax,
-                detail = forecastByDate[now] ?: emptyList()
-            )
+        val (current, forecast) = weatherList[0] to weatherList[1]
+        val groupedForecastList = forecast.list.groupBy { it.dtTxt.substringBefore(" ") }
+
+        val convertedList = mutableListOf<WeatherItem>()
+
+        convertedList.add(
+            convertToTodayWeather(current, groupedForecastList[now] ?: emptyList())
         )
 
-        for (key in forecastByDate.keys) {
+        for (key in groupedForecastList.keys) {
             if (key == now) continue
-            listItem.add(
-                WeatherItem(
-                    name = forecastList[0].name,
-                    date = key,
-                    icon = forecastByDate[key]?.get(0)?.icon ?: "",
-                    temp = forecastByDate[key]?.get(0)?.main?.temp ?: "",
-                    tempMin = forecastByDate[key]?.get(0)?.main?.tempMin ?: "",
-                    tempMax = forecastByDate[key]?.get(0)?.main?.tempMax ?: "",
-                    detail = forecastByDate[key] ?: emptyList()
-                )
+            convertedList.add(
+                convertToForecastWeather(key, groupedForecastList[key] ?: emptyList())
             )
         }
-        return listItem
+        return convertedList
     }
+
+    private fun convertToTodayWeather(current: Forecast, detail: List<ListItem>) =
+        WeatherItem(
+            name = current.name,
+            date = "Today",
+            icon = current.list[0].icon,
+            temp = current.list[0].main.temp,
+            tempMin = current.list[0].main.tempMin,
+            tempMax = current.list[0].main.tempMax,
+            detail = detail
+        )
+
+    private fun convertToForecastWeather(date: String, detail: List<ListItem>) =
+        WeatherItem(
+            date = date,
+            icon = detail[0].icon,
+            temp = detail[0].main.temp,
+            tempMin = detail[0].main.tempMin,
+            tempMax = detail[0].main.tempMax,
+            detail = detail
+        )
 }
