024105bba672aae5dd81c8dac7b10ccf105fa035
BeokBeok <kekemusa37@nate.com>
2020-06-17 21:49:23 +0900

    [적용] 날씨 이미지를 Glide 로 보여주기

--- app/src/main/java/com/example/beokweather/ext/BindingAdapterExt.kt
+++ app/src/main/java/com/example/beokweather/ext/BindingAdapterExt.kt
@@ -3,17 +3,18 @@ package com.example.beokweather.ext
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.beokweather.base.BaseAdapter
 
 @BindingAdapter("bind:setWeatherIcon")
 fun setWeatherIcon(imageView: ImageView, iconName: String?) {
     if (iconName.isNullOrEmpty()) return
 
-    imageView.run {
-        val convertPath = iconName.replace(iconName, "ic_weather_$iconName")
-        val imageId = context.resources.getIdentifier(convertPath, "drawable", context.packageName)
-        setImageDrawable(context.resources.getDrawable(imageId, null))
-    }
+    val iconUrl = "http://openweathermap.org/img/wn/$iconName@4x.png"
+    Glide.with(imageView)
+        .load(iconUrl)
+        .error(android.R.drawable.ic_menu_help)
+        .into(imageView)
 }
 
 @BindingAdapter("bind:replaceItems")
