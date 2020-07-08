12fb4d4fd6464f404bb08a52c739494696ad1286
BeokBeok <kekemusa37@nate.com>
2020-07-08 22:29:40 +0900

    [제거] 불필요한 Scope annotation

--- app/src/main/java/com/example/beokweather/di/RepositoryModule.kt
+++ app/src/main/java/com/example/beokweather/di/RepositoryModule.kt
@@ -7,14 +7,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
-import dagger.hilt.android.scopes.ActivityRetainedScoped
 
 @Module
 @InstallIn(ActivityRetainedComponent::class)
 class RepositoryModule {
 
     @Provides
-    @ActivityRetainedScoped
     fun provideWeatherRepository(weatherService: WeatherService): WeatherDataSource =
         WeatherRepository(weatherService)
 }
